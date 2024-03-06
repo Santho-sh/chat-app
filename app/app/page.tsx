3c28919b1d86cb8a8cd47f34dcccc6c2dea52481
Santhosh <santho-sh@outlook.com>
2024-03-06 16:33:45 +0530

    Add username prompt, users dropdown, toast message

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -10,89 +10,259 @@ import {
   CardHeader,
   CardContent,
   CardFooter,
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
   Input,
+  Label,
+  ScrollArea,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui";
+import { toast } from "sonner";
 
 type Message = {
-  username: string;
+  from: string;
+  to: string;
   content: string;
 };
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const username = "Santhosh";
+  const messageInputRef = useRef<HTMLInputElement>(null);
+  const usernameInputRef = useRef<HTMLInputElement>(null);
+  const [username, setUsername] = useState("");
   const [messages, setMessages] = useState<Message[]>([]);
-  const [socket, setSocket] = useState(() => {
-    return new WebSocket("ws://localhost:8080");
-  });
+  const [users, setUsers] = useState<string[]>([]);
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  // Open username prompt dialog on initial loading
+  useEffect(() => {
+    if (!username) {
+      setIsDialogOpen(true);
+    } else {
+      setIsDialogOpen(false);
+    }
+  }, [username]);
+
+  // Remove selected user when user is disconnected
+  useEffect(() => {
+    if (selectedUser !== null && !users.includes(selectedUser)) {
+      toast.error(`${selectedUser} disconnected`);
+      setSelectedUser(null);
+    }
+  }, [users]);
 
   useEffect(() => {
-    socket.onmessage = (event) => {
-      const message: Message = JSON.parse(event.data);
-      setMessages((messages) => {
-        return [...messages, message];
-      });
+    if (!socket) {
+      return;
+    }
+
+    // Get all active users names every 5 sec
+    const intervel = setInterval(() => {
+      socket.send(JSON.stringify({ action: "getAllUsers", from: username }));
+    }, 5000);
+
+    return () => {
+      clearInterval(intervel);
     };
   }, [socket]);
 
-  function SendMessage() {
-    if (inputRef.current) {
-      const content = inputRef.current.value.trim();
-      setMessages((messages) => {
-        return [...messages, message];
-      });
+  useEffect(() => {
+    if (!username) {
+      return;
+    }
 
-      const message = { username: username, content: content };
-      const data = JSON.stringify(message);
-      socket.send(data);
-      inputRef.current.value = "";
+    const webSocket = new WebSocket(`ws://localhost:8080?username=${username}`);
+
+    webSocket.onerror = (error) => {
+      console.log("error: ", error);
+    };
+
+    webSocket.onopen = (e) => {
+      console.log("connected");
+    };
+
+    webSocket.onclose = (e) => {
+      console.log("clossed: ", e);
+      setUsername("");
+      toast.error(e.reason);
+    };
+
+    // Handle recieving message
+    webSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (data.action === "getAllUsers") {
+        setUsers(data.users);
+      } else {
+        const message: Message = data;
+        setMessages((messages) => {
+          return [...messages, message];
+        });
+      }
+    };
+
+    setSocket(webSocket);
+
+    return () => {
+      webSocket.close();
+    };
+  }, [username]);
+
+  // Handle send message
+  function SendMessage() {
+    if (socket == null) {
+      return;
+    }
+
+    if (messageInputRef.current) {
+      if (selectedUser) {
+        const content = messageInputRef.current.value.trim();
+        if (!content) {
+          return;
+        }
+        setMessages((messages) => {
+          return [...messages, message];
+        });
+
+        const message = { from: username, to: selectedUser, content: content };
+        const data = JSON.stringify(message);
+        socket.send(data);
+        messageInputRef.current.value = "";
+      } else {
+        toast.error("Select which user to send the message to");
+      }
     }
   }
 
   return (
-    <main className="flex flex-col container gap-8 items-center justify-center">
-      <Card className="w-full h-[100vh] relative my-8">
-        <CardHeader className="flex flex-row items-center">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarFallback>{username.at(0)}</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">{username}</p>
+    <>
+      <Dialog open={isDialogOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Set Username</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                id="username"
+                ref={usernameInputRef}
+                defaultValue={username}
+                className="col-span-3"
+              />
             </div>
           </div>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-4">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={cn(
-                  "flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
-                  message.username === username
-                    ? "ml-auto bg-primary text-primary-foreground"
-                    : "bg-muted"
-                )}
+          <DialogFooter suppressHydrationWarning>
+            <DialogClose asChild suppressHydrationWarning>
+              <Button
+                type="submit"
+                onClick={() => {
+                  setUsername(usernameInputRef.current?.value || "");
+                }}
               >
-                {message.content}
+                Save
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      <div className="flex flex-col gap-8 items-center justify-center">
+        <Card className="w-full h-[90vh] relative my-[5vh]">
+          <CardHeader className="flex flex-row items-center">
+            <div className="flex w-full sm:justify-between sm:flex-row flex-col ">
+              <div className="flex items-center space-x-4">
+                <Avatar>
+                  <AvatarFallback>{username.at(0)}</AvatarFallback>
+                </Avatar>
+                <div>
+                  <p className="text-sm font-medium leading-none">{username}</p>
+                </div>
               </div>
-            ))}
-          </div>
-        </CardContent>
-        <CardFooter className="flex w-full items-center space-x-2">
-          <Input
-            id="message"
-            placeholder="Type your message..."
-            className="flex-1"
-            autoComplete="off"
-            ref={inputRef}
-          />
-          <Button onClick={SendMessage} size="icon">
-            <Send className="h-4 w-4" />
-            <span className="sr-only">Send</span>
-          </Button>
-        </CardFooter>
-      </Card>
-    </main>
+              <div>
+                <div className="flex items-center gap-3">
+                  <Label>Select User: </Label>
+                  <Select
+                    onValueChange={(value) => {
+                      setSelectedUser(value);
+                    }}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select user.." />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {users.length <= 1 ? (
+                          <SelectLabel>No active users</SelectLabel>
+                        ) : null}
+                        {users.map((user) =>
+                          user !== username ? (
+                            <SelectItem key={user} value={user}>
+                              {user}
+                            </SelectItem>
+                          ) : null
+                        )}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <ScrollArea className="h-[63vh] sm:h-[68vh] w-full">
+              <div className="space-y-4">
+                {messages.map((message, index) => (
+                  <div
+                    key={index}
+                    className={cn(
+                      "flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
+                      message.from === username
+                        ? "ml-auto bg-primary text-primary-foreground"
+                        : "bg-muted"
+                    )}
+                  >
+                    <p className="flex gap-2">
+                      {message.content}
+                      <span className="text-gray-400 text-[10px]">
+                        {message.from !== username
+                          ? `by ${message.from}`
+                          : `to ${message.to}`}
+                      </span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          </CardContent>
+          <CardFooter className="flex w-full items-center space-x-2 absolute bottom-0">
+            <Input
+              id="message"
+              placeholder="Type your message..."
+              className="flex-1"
+              autoComplete="off"
+              ref={messageInputRef}
+            />
+            <Button onClick={SendMessage} size="icon">
+              <Send className="h-4 w-4" />
+              <span className="sr-only">Send</span>
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
   );
 }
